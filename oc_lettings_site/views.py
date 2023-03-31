49c194015859e60a631f7c9a14642cded5c35a23
lcourdes <lea.courdes.murphy@gmail.com>
2023-03-31 12:57:07 +0200

    add new comments on views.py

--- oc_lettings_site/views.py
+++ oc_lettings_site/views.py
@@ -2,16 +2,19 @@ from django.shortcuts import render
 from .models import Letting, Profile
 
 
+# This view display the home page.
 def index(request):
     return render(request, 'index.html')
 
 
+# This view display the letting list page.
 def lettings_index(request):
     lettings_list = Letting.objects.all()
     context = {'lettings_list': lettings_list}
     return render(request, 'lettings_index.html', context)
 
 
+# This view display the letting detail of an address.
 def letting(request, letting_id):
     letting = Letting.objects.get(id=letting_id)
     context = {
@@ -21,12 +24,14 @@ def letting(request, letting_id):
     return render(request, 'letting.html', context)
 
 
+# This view display the profile list page.
 def profiles_index(request):
     profiles_list = Profile.objects.all()
     context = {'profiles_list': profiles_list}
     return render(request, 'profiles_index.html', context)
 
 
+# This view display the profile detail of a user.
 def profile(request, username):
     profile = Profile.objects.get(user__username=username)
     context = {'profile': profile}
