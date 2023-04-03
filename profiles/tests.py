278f9edab2c410edec05b16ee9bdb2a8c98452f5
lcourdes <lea.courdes.murphy@gmail.com>
2023-04-03 16:02:43 +0200

    add tests to profile

--- profiles/tests.py
+++ profiles/tests.py
@@ -1,3 +1,22 @@
-from django.test import TestCase
+import pytest
+from django.urls import reverse
 
-# Create your tests here.
+
+@pytest.fixture(scope='session')
+def django_db_setup():
+    django_db_use_migrations = False
+    return django_db_use_migrations
+
+
+@pytest.mark.django_db()
+def test_profile_index(client):
+    response = client.get(reverse('profiles:index'))
+    assert "<title>Profiles</title>" in response.content.decode()
+    assert response.status_code == 200
+
+
+@pytest.mark.django_db()
+def test_profile(client):
+    response = client.get(reverse('profiles:profile', args=["HeadlinesGazer"]))
+    assert "<h1>HeadlinesGazer</h1>" in response.content.decode()
+    assert response.status_code == 200
