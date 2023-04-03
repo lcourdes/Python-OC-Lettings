7ec53b9b2d6b0391020a521538260bff2c985620
lcourdes <lea.courdes.murphy@gmail.com>
2023-04-03 16:02:14 +0200

    remove test file in oc-lettings-site

--- oc_lettings_site/tests.py
+++
@@ -1,7 +0,0 @@
-from django.urls import reverse
-
-
-def test_index(client):
-    response = client.get(reverse('index'))
-    assert "Welcome to Holiday Homes" in response.content
-    assert response.status_code == 200
