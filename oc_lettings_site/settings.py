af1f25d10962cabbacf48ebe452dde59ff06c91a
lcourdes <lea.courdes.murphy@gmail.com>
2023-04-06 17:59:10 +0200

    GHA

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = 'fp$9^593hsriajg$_%=5trot9g!1qa@ew(o-1#@=&4%=hp46(s'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-#ALLOWED_HOSTS = ['127.0.0.1', 'ec2-15-236-202-82.eu-west-3.compute.amazonaws.com', 'localhost']
+# ALLOWED_HOSTS = ['127.0.0.1', 'ec2-15-236-202-82.eu-west-3.compute.amazonaws.com', 'localhost']
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split()
 
 # Application definition
