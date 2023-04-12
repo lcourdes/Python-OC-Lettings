a6980ee95aa36b68f400c8d3f4af2422392e9d32
lcourdes <lea.courdes.murphy@gmail.com>
2023-04-12 12:01:03 +0200

    change settings

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -26,7 +26,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'fp$9^593hsriajg$_%=5trot9g!1qa@ew(o-1#@=&4%=hp46(s'
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
@@ -35,9 +34,8 @@ if os.getenv('DEBUG') == 'True':
 else:
     DEBUG = False
 
-# ALLOWED_HOSTS = ['127.0.0.1', 'ec2-15-236-202-82.eu-west-3.compute.amazonaws.com', 'localhost']
-# ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(' ')
-ALLOWED_HOSTS = []
+if os.getenv('ALLOWED_HOSTS') is not None:
+    ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(' ')
 
 # Application definition
 
